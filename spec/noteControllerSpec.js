6229937ea6a237e0b0b0e88d69757e4c94f8f72b
Clare Pinder <Clare.pinder@live.co.uk>
2019-03-21 15:53:39 +0000

    failing test for noteController but functionality works

--- spec/noteControllerSpec.js
+++ spec/noteControllerSpec.js
@@ -5,16 +5,13 @@ function NoteDouble() {
 it("app div has no notes on load", function() {
   var noteController = new NoteController()
   noteController._setup()
-  console.log("doc app " + document.getElementById('app').child)
   assert.isEqual(document.getElementById('app').child, )
 });
 
 it("app div has note text on load", function() {
-  var noteController = new NoteController(noteList = new NoteList())
-  note = new Note('coffee time')
-  noteList.addNote(note)
+  var noteController = new NoteController(new NoteList())
+  note = new NoteDouble
+  noteController._noteList.addNote(note)
   noteController._setup()
-  console.log("doc app " + document.getElementById('app'))
-  console.log("doc app " + document.getElementById('app').child)
-  assert.isEqual(document.getElementById('app').child, )
+  assert.isEqual(document.getElementById('app').innerHTML, 'test note')
 });
